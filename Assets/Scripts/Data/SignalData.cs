60bb054fa756397c609df747a6477614d17c4a8a
Guillem Gracia Andreu <guillemgracia99@gmail.com>
2019-06-29 19:01:54 +0200

    SignalData and DataManager firsts steps

--- Assets/Scripts/Data/SignalData.cs
+++ Assets/Scripts/Data/SignalData.cs
@@ -1,8 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
 
 public class SignalData
 {
+    public enum SignalType { Point, Line };
 
+    TimeSpan startSignalTime;
+    TimeSpan endSignalTime;
+
+    public SignalData()
+    {
+        startSignalTime = DateTime.Now.TimeOfDay;
+        endSignalTime = DateTime.Now.TimeOfDay;
+    }
+
+    public SignalData(TimeSpan now)
+    {
+        startSignalTime = now;
+        endSignalTime = now;
+    }
+
+    public TimeSpan StartSignalTime
+    {
+        get { return startSignalTime; }
+    }
+
+    public TimeSpan EndSignalTime
+    {
+        get { return endSignalTime; }
+    }
+
+    public void EndSignal()
+    {
+        endSignalTime = DateTime.Now.TimeOfDay;
+    }
+
+    public double SignalDurationTime
+    {
+        get { return endSignalTime.TotalSeconds - startSignalTime.TotalSeconds; }
+    }
+
+    public double SignalDurationTimeMili
+    {
+        get { return endSignalTime.TotalMilliseconds - startSignalTime.TotalMilliseconds; }
+    }
 }
--- Assets/Scripts/Singletons/DataManager.cs
+++ Assets/Scripts/Singletons/DataManager.cs
@@ -4,8 +4,27 @@ using UnityEngine;
 
 public class DataManager : Singleton<DataManager>
 {
+    public enum SignalSpace { Letter, Word};
+
+    List<SignalData> signalList;
+
     public void Init_DataManager()
     {
+        signalList = new List<SignalData>();
+    }
 
+    public double TimeBetweenTwoSignals(SignalData data1, SignalData data2)
+    {
+        return data2.StartSignalTime.TotalSeconds - data1.EndSignalTime.TotalSeconds;
+    }
+
+    public void AddSignal()
+    {
+        signalList.Add(new SignalData());
+    }
+
+    public void EndSignal()
+    {
+        signalList[signalList.Count - 1].EndSignal();
     }
 }
