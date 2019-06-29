c112adbb270813b84c4440d95049da89d44f9002
Guillem Gracia Andreu <guillemgracia99@gmail.com>
2019-06-29 18:32:29 +0200

    Some Scripts

---
+++ Assets/Scripts/Singletons/DataManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : Singleton<DataManager>
+{
+    public void Init_DataManager()
+    {
+
+    }
+}
